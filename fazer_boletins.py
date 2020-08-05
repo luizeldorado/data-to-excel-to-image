578e5ff130e6ad47c272e3c7049b5ae18f560cd6
Luiz Pontes <eldorado.luiz@gmail.com>
2020-08-05 18:34:25 -0300

    Changed prints to logs Default to removing excel files Added requirements.txt

--- fazer_boletins.py
+++ fazer_boletins.py
@@ -1,7 +1,7 @@
 # Warning: shit coding ahead
 
 make_image_files = True
-remove_excel_files = False
+remove_excel_files = True
 
 data_filename = "dados.xlsx"
 template_filename = "modelo.xlsx"
@@ -12,44 +12,62 @@ image_prefix = "boletim "
 
 '''
 TODO:
-- Search for tags only once in template, instead of each time. Like, save the cells and positions of tags and just use them later.
+- Search for tags only once in template, instead of each time. Like, save the cells and positions of tags and just use them later. (would still have to reload it to reset to original template content)
 - Remove excel2img, since it reopens the file. Make it so we use openpyxl, maybe in conjunction with win32 API.
 - Save and restore clipboard state. Yep, it actually puts stuff in there to save the image. I don't think that there is a function on the API to save the image directly.
-- Put all them prints in a disableable debug function
 - Make those options above into command line args
 - Maybe make a GUI
+- Be able to select multiple worksheets in the template file, each one would make one file
 - Fix/add other things that I can't remember right now
+
 '''
 
 import excel2img
 import openpyxl
 import os
 from collections import namedtuple
+import logging
+from logging import debug, info, warning, error, critical
 
-fields = []
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-print("Opening data file '{}'".format(data_filename))
+stop = False
+
+if not os.path.isfile(data_filename):
+	critical("Data file '{}' does not exist!".format(data_filename))
+	stop = True
+if not os.path.isfile(template_filename):
+	critical("Template file '{}' does not exist!".format(template_filename))
+	stop = True
+
+if stop:
+	exit()
+
+debug("Opening data file '{}'".format(data_filename))
 
 wb = openpyxl.load_workbook(data_filename)
 ws = wb.active
 
-print("Getting field names:")
+debug("Getting field names:")
 
+fields = []
 for cell in ws[1]:
 	fields.append(str(cell.value))
 
-print(fields)
+debug(fields)
 
-print("Getting each data row:")
+debug("Getting each data row:")
 
 for data_row in ws.iter_rows(min_row=2):
 
-	print("- (Re)opening template file '{}'".format(template_filename))
+	info("Generating row '{}'".format(str(data_row[0].value)))
+
+	debug("- (Re)opening template file '{}'".format(template_filename))
 
 	wb = openpyxl.load_workbook(template_filename)
 	ws = wb.active
 
-	print("- Looping through all non-empty cells to find tags:")
+	debug("- Looping through all non-empty cells to find tags:")
 
 	for template_row in ws.rows:
 		for cell in template_row:
@@ -60,7 +78,7 @@ for data_row in ws.iter_rows(min_row=2):
 				cell_str = str(cell.value)
 				pos = 0
 
-				print("  - Cell {}: {}".format(cell.coordinate, cell_str))
+				debug("  - Cell {}: {}".format(cell.coordinate, cell_str))
 
 				while True:
 
@@ -72,13 +90,13 @@ for data_row in ws.iter_rows(min_row=2):
 						percent_pos_end = cell_str.find("%", percent_pos_start+1)
 
 						if percent_pos_end == -1:
-							print("    - percent_pos_start = {}, no ending percent".format(percent_pos_start))
+							debug("    - percent_pos_start = {}, no ending percent".format(percent_pos_start))
 							break  # no ending %, so there is no more tags (no point in warning, probably an actual percentage sign there
 
 						# find name of tag
 						tag_name = cell_str[percent_pos_start+1:percent_pos_end]
 
-						print("    - percent_pos_start = {}, percent_pos_end = {}, tag_name = {}".format(percent_pos_start, percent_pos_end, tag_name))
+						debug("    - percent_pos_start = {}, percent_pos_end = {}, tag_name = {}".format(percent_pos_start, percent_pos_end, tag_name))
 
 						# find which field is this tag
 						for field_i, field in enumerate(fields):
@@ -90,7 +108,7 @@ for data_row in ws.iter_rows(min_row=2):
 								data_value = data_row[field_i].value
 								data_type = data_row[field_i].data_type
 
-								print("      - data_value = {}, data_type = {}".format(repr(data_value), data_type), )
+								debug("      - data_value = {}, data_type = {}".format(repr(data_value), data_type), )
 
 								data_value_str = str(data_value) if data_value != None else ""
 
@@ -102,7 +120,7 @@ for data_row in ws.iter_rows(min_row=2):
 									cell.value = data_value
 									# cell.data_type = data_type
 
-									print("        - whole tag cell copied directly, cell.data_type = {}".format(cell.data_type))
+									debug("        - whole tag cell copied directly, cell.data_type = {}".format(cell.data_type))
 
 								else:
 
@@ -111,15 +129,16 @@ for data_row in ws.iter_rows(min_row=2):
 
 								pos = percent_pos_start + len(data_value_str)
 
-								print("      - replaced, cell_str = {}".format(cell_str))
+								debug("      - replaced, cell_str = {}".format(cell_str))
 
 								break
 
 						else:
-							print("      - no field called {}".format(tag_name))
+							debug("      - no field called {}".format(tag_name))
+							warning("%{}% found in template file, but not in the data file. Did you mispell it or didn't add it to the template?".format(tag_name))
 							pos = percent_pos_end+1
 
-						print("      - pos = {}".format(pos))
+						debug("      - pos = {}".format(pos))
 
 					else:
 						# no more %s
@@ -128,14 +147,14 @@ for data_row in ws.iter_rows(min_row=2):
 	excel_filename = excel_prefix + str(data_row[0].value) + ".xlsx"
 	image_filename = image_prefix + str(data_row[0].value) + ".png"
 
-	print("- Saving excel file {}".format(excel_filename))
+	debug("- Saving excel file {}".format(excel_filename))
 
 	wb.save(excel_filename)
 
 	if make_image_files:
-		print("- Saving image file {}".format(image_filename))
-		excel2img.export_img(excel_filename, image_filename, "Plan1", None)
+		debug("- Saving image file {}".format(image_filename))
+		excel2img.export_img(excel_filename, image_filename, None, None)
 
 	if remove_excel_files:
-		print("- Removing excel file {}".format(excel_filename))
+		debug("- Removing excel file {}".format(excel_filename))
 		os.remove(excel_filename)
